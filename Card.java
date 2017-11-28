aad6bcbb3e2756a2662e77c95cea94a2bdfe5c3b
5871jin <33578781+5871jin@users.noreply.github.com>
2017-11-28 13:20:08 -0500

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,32 @@
+package cmsc131.cards;
+
+/**
+ * A Card represents an immutable playing card. For example, a card object that
+ * represents the five of hearts will always represent the five of hearts.
+ * 
+ * You need to define the appropriate instance fields and implement the
+ * following methods.
+ * 
+ * You also need to define methods to allow Card objects to be compared for
+ * equality, and some kind of useful toString method. Feel free/encouraged to
+ * use the Source -> Generate... methods to generate these methods.
+ *
+ */
+public class Card {
+
+  /** Initial a new card with the specified rank and suit */
+  public Card(Rank rank, Suit suit) {
+    throw new UnsupportedOperationException("Replace with your own implementation");
+  };
+
+  /** Return the rank of this card */
+  public Rank getRank() {
+    throw new UnsupportedOperationException("Replace with your own implementation");
+  }
+
+  /** Return the rank of this card */
+  public Suit getSuit() {
+    throw new UnsupportedOperationException("Replace with your own implementation");
+  }
+
+}
